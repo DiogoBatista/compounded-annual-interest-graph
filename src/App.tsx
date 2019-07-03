d4ee06e7bec63b666aaee7b1a650c220f12ba4a1
diogo batista <diogo@themainingredient.co>
2019-07-03 21:47:43 +0200

    added gcloud deploy files and footer

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,8 @@ export const App = () => {
             }
           }]
         },
+        responsive: true,
+        maintainAspectRatio: false,
         legend: {
           position: 'bottom'
         },
@@ -68,49 +70,66 @@ export const App = () => {
   }
 
   return (
-    <div className="container">
-      <h1 className="title">Compound Interests with monthly contributions</h1>
-      <div className="field is-grouped">
-        <div className="control">
-          <label className="label">Years</label>
-          <input className="input"
-            min="0"
-            type="number"
-            placeholder="years"
-            value={years}
-            onChange={(e) => handleYearsChange(parseInt(e.target.value))} />
+    <>
+      <div className="container">
+        <h1 className="title">Compound Interests with monthly contributions</h1>
+        <div className="field is-grouped">
+          <div className="control">
+            <label className="label">Years</label>
+            <input className="input"
+              min="0"
+              type="number"
+              placeholder="years"
+              value={years}
+              onChange={(e) => handleYearsChange(parseInt(e.target.value))} />
+          </div>
+          <div className="control">
+            <label className="label">Initial Deposit</label>
+            <input className="input"
+              min="0"
+              type="number"
+              placeholder="Initial deposit $"
+              value={initialDeposit}
+              onChange={(e) => handleInitialDepositChange(parseInt(e.target.value))} />
+          </div>
+          <div className="control">
+            <label className="label">Monthly Contributions</label>
+            <input className="input"
+              min="0"
+              type="number"
+              placeholder="Monthly contributions $"
+              value={monthlyContributions}
+              onChange={(e) => handleMonthlyContributionsChange(parseInt(e.target.value))} />
+          </div>
+          <div className="control">
+            <label className="label">Annual Interest rate</label>
+            <input className="input"
+              type="number"
+              placeholder="Annual Interest rate %"
+              value={annualInterestRate}
+              onChange={(e) => handleAnnualInterestRateChange(parseInt(e.target.value))} />
+          </div>
         </div>
-        <div className="control">
-          <label className="label">Initial Deposit</label>
-          <input className="input"
-            min="0"
-            type="number"
-            placeholder="Initial deposit $"
-            value={initialDeposit}
-            onChange={(e) => handleInitialDepositChange(parseInt(e.target.value))} />
-        </div>
-        <div className="control">
-          <label className="label">Monthly Contributions</label>
-          <input className="input"
-            min="0"
-            type="number"
-            placeholder="Monthly contributions $"
-            value={monthlyContributions}
-            onChange={(e) => handleMonthlyContributionsChange(parseInt(e.target.value))} />
-        </div>
-        <div className="control">
-          <label className="label">Annual Interest rate</label>
-          <input className="input"
-            type="number"
-            placeholder="Annual Interest rate %"
-            value={annualInterestRate}
-            onChange={(e) => handleAnnualInterestRateChange(parseInt(e.target.value))} />
+        <div className="container">
+          <canvas id="chart" width="400" height="400"></canvas>
         </div>
       </div>
-      <div>
-        <canvas id="chart" width="400" height="250"></canvas>
-      </div>
-    </div>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            Hello, my name is Diogo Batista and I'm a software developer.
+          </p>
+          <p>
+            If you like this small tool, check out my linkedin: <a href="https://www.linkedin.com/in/diogobvbatista/" target="_blank">Diogo Batista</a> <br />
+            I like to share my small tools and tests with you all 😎
+          </p>
+          <p>
+            Check out the medium articles for this tool
+            <a href="https://www.linkedin.com/in/diogobvbatista/" target="_blank">Diogo Batista</a>
+          </p>
+        </div>
+      </footer>
+    </>
   )
 }
 
